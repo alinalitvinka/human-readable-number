beb1b1954f2f05cdb2e281aad976756a2596069f
alinalitvinka <alinalitvinko7@gmail.com>
2020-10-18 01:26:58 +0300

    fix: marks

--- src/index.js
+++ src/index.js
@@ -1,125 +1,25 @@
 module.exports = function toReadable (number) {
+  const arrNum = ['','one','two','three','four','five','six','seven','eight','nine'];
+  const arrTwenty = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  const arrDozen = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
   let string = '';
-  if (number.length === 3 || number.length === 1) {
-    switch (number[0]) {
-        case 1:
-          string += 'one';
-          break;
-        case 2:
-          string += 'two';
-          break;
-        case 3:
-          string += 'three';
-          break;
-        case 4:
-          string += 'four';
-          break;
-        case 5:
-          string += 'five';
-          break;
-        case 6:
-          string += 'six';
-          break;
-        case 7:
-          string += 'seven';
-          break;  
-        case 8:
-          string += 'eight';
-          break;
-        case 9:
-            string += 'nine';
-            break;
-  }};
-  if (number.length === 3) {
-      string += 'hundred ';
-  };
-  if ((number.length === 3 || number.length === 2) && number[number.length-2] === 1) {
-    switch (number[number.length-1]) {
-        case 0:
-            string += 'ten';
-          break;
-        case 1:
-          string += 'eleven';
-          break;
-        case 2:
-          string += 'twelve';
-          break;
-        case 3:
-          string += 'thirteen';
-          break;
-        case 4:
-          string += 'fourteen';
-          break;
-        case 5:
-          string += 'fifteen';
-          break;
-        case 6:
-          string += 'sixteen';
-          break;
-        case 7:
-          string += 'seventeen';
-          break;  
-        case 8:
-          string += 'eighteen';
-          break;
-        case 9:
-            string += 'nineteen';
-            break;
-  }} else if (number.length === 3 || number.length === 2) {
-      switch (number[number.length-2]) {
-        case 2:
-            string += 'twenty';
-            break;
-        case 3:
-            string += 'thirty';
-            break;
-        case 4:
-            string += 'forty';
-            break;
-        case 5:
-            string += 'fifty';
-            break;
-        case 6:
-            string += 'sixty';
-            break;
-        case 7:
-            string += 'seventy';
-            break;
-        case 8:
-            string += 'eighty';
-            break;
-        case 9:
-            string += 'ninety';
-            break;
-    }};
-  if ((number.length === 3 || number.length === 2) && number[number.length-1] !== 0) {
-      switch (number[number.length-1]) {
-        case 1:
-            string += ' one';
-            break;
-        case 2:
-            string += ' two';
-            break;
-        case 3:
-            string += ' three';
-            break;
-        case 4:
-            string += ' four';
-            break;
-        case 5:
-            string += ' five';
-            break;
-        case 6:
-            string += ' six';
-            break;
-        case 7:
-            string += ' seven';
-            break;  
-        case 8:
-            string += ' eight';
-            break;
-        case 9:
-            string += ' nine';
-            break;
-    }}; return string;
-};
+  let num = number.toString().split('');
+  let a = num.length;
+  if (num[0] === '0') { string += 'zero'; }
+  if (a === 3 || a === 1) {
+    string += arrNum[num[0]];
+  }
+  if (a === 3) {
+      string += ' hundred';
+  }
+  if (a === 3 && (num[a-2] !== '0' || num[a-1] !== '0')) { string += ' ';}
+  if (a !== 1 && num[a-2] === '1') {
+    string += arrTwenty[num[a-1]];        
+  } else if (a === 3 || a === 2) {
+     string += arrDozen[num[a-2]];     
+  }
+  if (a !== 1 && num[a-2] !== '0' && num[a-1] !== '0' && num[a-2] !== '1') { string += ' ';}
+  if (a !== 1 && num[a-1] !== '0' && num[a-2] !== '1') {
+    string += arrNum[num[a-1]];
+  } return string;
+}
